4339d9fbab897e61d48f82b8ff5e93b07ed4ddef
Kalsha Samarajeewa <kalsha@Kalshas-MacBook-Air.local>
2023-08-29 11:51:11 +0530

    Update requirements.txt

--- app/models.py
+++ app/models.py
@@ -1,8 +1,30 @@
+# from app import db
+# from pgvector.sqlalchemy import Vector
+# from sqlalchemy import Integer, String, Text
+# from sqlalchemy.orm import declarative_base, mapped_column
+
+
+# Base = declarative_base()
+
+# class Document(Base):
+#     __tablename__ = "document"
+
+#     id = mapped_column(Integer, primary_key=True)
+#     title = mapped_column(String)
+#     content = mapped_column(Text)
+#     embedding = mapped_column(Vector(384))
+
+# Base.metadata.create_all(engine)
+
+
 from app import db
 from pgvector.sqlalchemy import Vector
 from sqlalchemy import Integer, String, Text
 from sqlalchemy.orm import declarative_base, mapped_column
+from sqlalchemy import create_engine
+from app.config import SQLALCHEMY_DATABASE_URI
 
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
 Base = declarative_base()
 
@@ -14,3 +36,7 @@ class Document(Base):
     title = mapped_column(String)
     content = mapped_column(Text)
     embedding = mapped_column(Vector(384))
+
+
+# Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
