63cb8707593b6cd7de912f62c9dd0a8ecbd1c5cc
Sathya Karunakalage <82775587+sathyaudayangi@users.noreply.github.com>
2023-11-16 13:23:01 +0530

    Fix issues in loop

--- app/routes.py
+++ app/routes.py
@@ -402,10 +402,7 @@ def search_similar_resource():
     user_id = data.get("user_id")
 
     topic_id = (db.session.query(models.Document.topic_id).filter(models.Document.id == doc_id).first())[0]
-
     course_id = (db.session.query(models.Topic.course_id).filter(models.Topic.id == topic_id).first())[0]
-
-    # print(course_id)
 
     similar_topic_ids = (db.session.query(models.Topic.id).filter(models.Topic.course_id == course_id).all())
     similar_topic_ids = [topic_id for (topic_id,) in similar_topic_ids]
@@ -414,7 +411,7 @@ def search_similar_resource():
         db.session.query(models.Document.id)
         .filter(
             models.Document.topic_id.in_(similar_topic_ids),
-            models.Document.id != doc_id,
+            models.Document.id != doc_id
         )
         .all()
     )
@@ -428,29 +425,23 @@ def search_similar_resource():
             models.DocSimilarity.existing_document_id != models.DocSimilarity.new_document_id,
         )
         .order_by(models.DocSimilarity.similarity_score.desc())
-        .slice(0, 5)
         .all()
     )
 
     all_users = db.session.query(models.User.id).count()
 
-    i = 0
-    isOwner = False
-    response_dict = {}
-    for result in doc_results:
-        document_owners = db.session.query(models.OwnsDocument.user_id).filter(models.OwnsDocument.document_id == result.existing_document_id).all()
-        for owner_id in document_owners:
-            if (owner_id[0] == user_id): 
-                isOwner = True
-                break
-        if(isOwner):
-            isOwner = False 
+    response_dict = []
+    for count, result in enumerate(doc_results):
+        document_owners = [owner_id[0] for owner_id in db.session.query(models.OwnsDocument.user_id).filter(models.OwnsDocument.document_id == result.existing_document_id).all()]
+        
+        if user_id in document_owners:
             continue
+        
         user_count = (db.session.query(models.OwnsDocument).filter(models.OwnsDocument.document_id == result.existing_document_id).count())
         recommending_doc_ratings = (db.session.query( models.Document.ratings).filter(models.Document.id == result.existing_document_id).scalar())
         recommending_doc_title = (db.session.query(models.Document.title).filter(models.Document.id == result.existing_document_id).scalar())
-        resource_topic = (db.session.query(models.Topic).filter(models.Topic.id == models.Document.topic_id, models.Document.id == result.existing_document_id).first())
-        response_dict[i] = {
+        resource_topic = (db.session.query(models.Topic).filter(models.Topic.id == models.Document.topic_id, models.Document.id == result.existing_document_id).first())       
+        response_dict.append({
             "document_id": result.existing_document_id,
             "document_title": recommending_doc_title,
             "topic_id": resource_topic.id,
@@ -459,14 +450,13 @@ def search_similar_resource():
             "similarity_score": result.similarity_score,
             "user_count": user_count,
             "similarity_weight": (user_count / all_users) * result.similarity_score * (recommending_doc_ratings / 5)
-        }
-        i = i + 1
+        })
 
-    response_data = sorted(
-        response_dict.values(), key=lambda i: i["similarity_weight"], reverse=True
-    )
+        if len(response_dict) == 5: 
+            print("Breaking the loop at count =", count)
+            break
 
-    return {"results": response_data}
+    return {"results": response_dict}
 
 @app.route("/resource-info", methods=["GET"])
 def get_resource_info():
