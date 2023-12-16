cef555b62b1879d1b1ecb9b45f4d3a6c95c46367
Dulaj Prabasha <shad3s.of.m3@gmail.com>
2023-12-16 21:06:22 +0530

    fix recommendation route issues

--- app/run.py
+++ app/run.py
@@ -3,5 +3,5 @@ from app.config import APP_PORT
 import os
 
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
+    app.secret_key = "REPLACE_ME"
     app.run(debug=True, port=APP_PORT)
