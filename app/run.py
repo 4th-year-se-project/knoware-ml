b8eaf8261c946f73744c10548398fa55f3c89871
Dulaj Prabasha <shad3s.of.m3@gmail.com>
2023-12-20 19:06:37 +0530

    revert config and secret changes

--- app/run.py
+++ app/run.py
@@ -3,5 +3,5 @@ from app.config import APP_PORT
 import os
 
 if __name__ == "__main__":
-    app.secret_key = "REPLACE_ME"
+    app.secret_key = os.urandom(12)
     app.run(debug=True, port=APP_PORT)
